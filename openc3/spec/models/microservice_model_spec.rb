4a891dfa623df565f1a31c929f14ea2484858164
Matthew Cosgriff <matthew@openc3.com>
2026-02-19 14:36:26 -0700

    When looking for python binary to replace, need to handle single string and one word per parameter modes (#2851)
    * When replacing the python binary need to handle CMD python start.py and CMD "python start.py"
    
    * fix python unit test
    
    ---------
    
    Co-authored-by: Ryan Melton <ryan@openc3.com>

--- openc3/spec/models/microservice_model_spec.rb
+++ openc3/spec/models/microservice_model_spec.rb
@@ -14,7 +14,7 @@
 # GNU Affero General Public License for more details.
 
 # Modified by OpenC3, Inc.
-# All changes Copyright 2022, OpenC3, Inc.
+# All changes Copyright 2026, OpenC3, Inc.
 # All Rights Reserved
 #
 # This file may also be used under the terms of a commercial license
@@ -172,6 +172,36 @@ module OpenC3
         expect(json['target_names']).to include("TARGET1", "TARGET2")
         expect(json['cmd']).to eql ["ruby", "run.rb", "--switch"]
         expect(json['options']).to include(["NAME1", "VALUE1"], ["NAME2", "VALUE2"])
+        tf.unlink
+      end
+
+      it "replaces python executable in CMD with OPENC3_PYTHON_BIN" do
+        ['python', 'python3', 'python3.11', 'python3.11.2'].each do |exe|
+          model = MicroserviceModel.new(folder_name: "TEST", name: "DEFAULT__TYPE__NAME", scope: "DEFAULT")
+          parser = ConfigParser.new
+          tf = Tempfile.new
+          tf.puts "CMD #{exe} start.py"
+          tf.close
+          parser.parse_file(tf.path) do |keyword, params|
+            model.handle_config(parser, keyword, params)
+          end
+          expected_bin = ENV['OPENC3_PYTHON_BIN'] || '/openc3/python/.venv/bin/python'
+          expect(model.as_json()['cmd']).to(eql([expected_bin, "start.py"]), "failed for exe: #{exe}")
+          tf.unlink
+        end
+      end
+
+      it "replaces python executable in CMD when given as a quoted single string" do
+        model = MicroserviceModel.new(folder_name: "TEST", name: "DEFAULT__TYPE__NAME", scope: "DEFAULT")
+        parser = ConfigParser.new
+        tf = Tempfile.new
+        tf.puts "CMD 'python start.py'"
+        tf.close
+        parser.parse_file(tf.path) do |keyword, params|
+          model.handle_config(parser, keyword, params)
+        end
+        expected_bin = ENV['OPENC3_PYTHON_BIN'] || '/openc3/python/.venv/bin/python'
+        expect(model.as_json()['cmd']).to eql [expected_bin, "start.py"]
         tf.unlink
       end
 
