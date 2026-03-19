faaf77c2975400f6c1b03859302c750e41d5aa27
Jason Thomas <jason@openc3.com>
2026-03-19 08:52:42 -0600

    Merge pull request #2985 from OpenC3/local_mode
    Guard against OPENC3_LOCAL_MODE_PATH not existing

--- openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/admin/tabs/plugins/PluginList.vue
+++ openc3-cosmos-init/plugins/packages/openc3-vue-common/src/tools/admin/tabs/plugins/PluginList.vue
@@ -26,6 +26,7 @@
   >
     <template #item="{ item: plugin }">
       <plugin-list-item
+        :key="plugin.name"
         v-bind="plugin"
         :targets="pluginTargets(plugin.name)"
         :is-modified="isModified(plugin.name)"
